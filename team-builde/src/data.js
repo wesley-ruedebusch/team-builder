050766f67c7ac4054060e25a468f5147bae4ffa9
Wesley Ruedebusch <w.ruedebusch@gmail.com>
2020-01-29 20:08:46 -0500

    mvp meet

---
+++ team-builde/src/data.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name: 'Peter Griffin',
+        email: 'pgriffin@gmail.com',
+        role: 'Back-End Engineer',
+    },
+    {
+        id: 2,
+        name: 'Lois Griffin',
+        email: 'lgriffin@gmail.com',
+        role: 'Peters Wife',
+    },
+    {
+        id: 3,
+        name: 'Stewie Griffin',
+        email: 'sgriffin@gmail.com',
+        role: 'Peter and Lois Son',
+    },
+]
+
+export default members 
